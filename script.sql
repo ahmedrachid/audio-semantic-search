bfd95bc4a2aaddac832cd99cc4cab7d8c5b14851
Ahmed Rachid <ahmedrachidhazourli@yahoo.fr>
2023-09-17 16:50:27 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,4 +1,5 @@
 create database demo_gpdb;
+create extension if not exists vector;
 \c demo_gpdb;
 set optimizer to off;
 create table public.audio (index bigint, ids text, artist text, genre text, name text, subgenres text, urls text, audio double precision[], panns_embeddings vector(2048));
