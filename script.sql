301797c1e8671f60cfe0d7bc6c3e7e32b4906047
Ahmed Rachid <ahmedrachidhazourli@yahoo.fr>
2023-09-17 21:36:53 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -2,5 +2,8 @@ create database demo_gpdb;
 create extension if not exists vector;
 \c demo_gpdb;
 set optimizer to off;
+create table audio_embeddings(index int, ids text, panns_embeddings vector(2048), audio  double precision[]);
+create table audio_metadata(index bigint, ids text, artist text, genre text, name text, subgenres text, urls text);
 create table public.audio (index bigint, ids text, artist text, genre text, name text, subgenres text, urls text, audio double precision[], panns_embeddings vector(2048));
+insert into audio select a.*, b.audio, b.panns_embeddings from audio_embeddings b left join audio_metadata a on b.ids = a.ids;
 copy public.audio from '/home/gpadmin/audio.csv' CSV HEADER DELIMITER '|' QUOTE '"' NULL '';  
